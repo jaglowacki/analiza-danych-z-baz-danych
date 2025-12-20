ca9289ffe631cb1caa1058d97628aed550810a79
Janusz Głowacki <jglowacki@int.pl>
2025-12-20 14:50:19 +0100

    added solution of zad_6 i zad_7

--- Cwiczenia_4.sql
+++ Cwiczenia_4.sql
@@ -35,3 +35,47 @@ JOIN Categories C ON P.CategoryID=C.CategoryID ORDER BY 1
 SELECT C.CategoryName, COUNT(P.ProductID) AS 'Ile produktów w kategorii:'
 FROM Products P JOIN Categories C ON P.CategoryID=C.CategoryID 
 GROUP BY C.CategoryName ORDER BY 1
+
+--6. 
+SELECT DISTINCT C.CompanyName, P.ProductName FROM Customers C
+JOIN Orders O ON C.CustomerID=O.CustomerID 
+JOIN [Order Details] OD ON O.OrderID=OD.OrderID
+JOIN Products P ON OD.ProductID=P.ProductID ORDER BY 1,2
+
+SELECT DISTINCT C.CompanyName, P.ProductName FROM Customers C
+JOIN Orders O ON C.CustomerID=O.CustomerID 
+JOIN [Order Details] OD ON O.OrderID=OD.OrderID
+JOIN Products P ON OD.ProductID=P.ProductID 
+GROUP BY C.CompanyName, P.ProductName ORDER BY 1,2
+
+--7.
+SELECT * FROM Employees
+SELECT * FROM Orders
+SELECT * FROM [Order Details]
+
+SELECT E.EmployeeID, E.LastName+' '+E.FirstName AS 'Pracownik', 
+SUM(OD.UnitPrice*OD.Quantity) AS 'WS bez rabatu',
+SUM(OD.UnitPrice*OD.Quantity*OD.Discount) AS 'Rabaty', 
+SUM(OD.UnitPrice*OD.Quantity*(1-OD.Discount)) AS 'WS z rabatem'
+FROM Employees E
+JOIN Orders O ON E.EmployeeID=O.EmployeeID
+JOIN [Order Details] OD ON O.OrderID=OD.OrderID
+GROUP BY E.EmployeeID, E.LastName,E.FirstName ORDER BY 3 DESC
+
+SELECT E.EmployeeID, E.LastName+' '+E.FirstName AS 'Pracownik', 
+SUM(OD.UnitPrice*OD.Quantity) AS 'WS bez rabatu',
+CONVERT(DECIMAL(10,2), SUM(OD.UnitPrice*OD.Quantity*OD.Discount)) AS 'Rabaty', 
+CONVERT(DECIMAL(10,2), SUM(OD.UnitPrice*OD.Quantity*(1-OD.Discount))) AS 'WS z rabatem'
+FROM Employees E
+JOIN Orders O ON E.EmployeeID=O.EmployeeID
+JOIN [Order Details] OD ON O.OrderID=OD.OrderID
+GROUP BY E.EmployeeID, E.LastName, E.FirstName ORDER BY 3 DESC
+
+SELECT o.EmployeeID, E.LastName + ' ' + E.FirstName AS 'Pracownik'
+, SUM(od.unitprice* od.quantity) AS 'WS'
+, SUM(od.unitprice* od.quantity*od.discount) AS 'Rabat'
+, SUM(od.unitprice* od.quantity) - SUM(od.unitprice* od.quantity*od.discount) AS 'Wartoœæ bez rabatu'
+FROM Employees E JOIN Orders O on E.EmployeeID = O.EmployeeID
+JOIN [Order Details] OD on OD.OrderID = O.OrderID
+GROUP BY E.LastName, E.FirstName, O.EmployeeID
+ORDER BY SUM(od.unitprice* od.quantity) desc
