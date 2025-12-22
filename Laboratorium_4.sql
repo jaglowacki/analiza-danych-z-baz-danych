256ee356a8feb1e0601733b4f9cf91089e3c354e
Janusz Glowacki <jglowacki@int.pl>
2025-12-22 14:21:24 +0100

    added zad9 with UNION ALL

--- Laboratorium_4.sql
+++ Laboratorium_4.sql
@@ -9,4 +9,15 @@ INNER JOIN [Categories] ON Products.CategoryID=Categories.CategoryID --INNER jes
 SELECT ProductName, CategoryName FROM [Products] 
 JOIN [Categories] ON Products.CategoryID=Categories.CategoryID
 
-
+--9. Wyznacz wiek pracowników.
+--Który z nich jest najstarszy, a który najm³odszy?
+-- wybór tylko pracownika najstarszego i najm³odszego
+select * from (SELECT top 1 'tabela 1' as typ, FirstName, LastName, BirthDate,
+DATEDIFF (yy, BirthDate, GETDATE() ) AS Wiek
+FROM Employees
+order by Wiek) as najmlodszy
+UNION all
+select * from (SELECT  top 1 'tabela 2' as typ, FirstName, LastName, BirthDate,
+DATEDIFF (yy, BirthDate, GETDATE() ) AS Wiek
+FROM Employees
+order by Wiek desc) as najstarszy
