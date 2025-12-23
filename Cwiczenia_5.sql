3c96f0be71d17e888455888917454e863448707e
Janusz Glowacki <jglowacki@int.pl>
2025-12-23 08:57:39 +0100

    added zad3

--- Cwiczenia_5.sql
+++ Cwiczenia_5.sql
@@ -18,4 +18,21 @@ ELSE 'Do weryfikacji'
 END AS 'Stanowisko'
 FROM Employees
 
+--3. 
+SELECT ProductID, ProductName, UnitPrice,
+CASE
+WHEN UnitPrice<20 THEN 'Ni¿sza cena'
+WHEN UnitPrice BETWEEN 20 AND 40 THEN 'Œrednia cena'
+WHEN UnitPrice>40 THEN 'Wy¿sza cena'
+ELSE 'Nieznana cena'
+END AS 'Poziom ceny'
+FROM Products
 
+--3b.
+SELECT ProductID, ProductName, UnitPrice,
+IIF(UnitPrice<20, 'Ni¿sza cena',
+	IIF (UnitPrice BETWEEN 20 AND 40,'Œrednia cena',
+		IIF (UnitPrice>40, 'Wy¿sza cena','Nieznana cena'))) AS 'Poziom ceny'
+FROM Products P
+
+SELECT ProductName  FROM Products
